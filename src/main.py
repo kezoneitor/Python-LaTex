232a644a643ac801ccd93be0b33d509d698fc54d
Keslerth <keslerth.c@gmail.com>
2023-02-25 11:29:48 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
-print("Hello world!")
-
 testingVar = ""
 if(testingVar == ""):
     print("Yes")
 else:
     print("No")
+
+print("Hello world!")
+
+function main():
+    num1 = 10
+    num2 = 5
+    print(num1+num2)
